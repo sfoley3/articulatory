4903b3e62084f02b58ac08782dac8673c5983da3
sfoley3 <64561458+sfoley3@users.noreply.github.com>
2023-09-04 14:03:19 -0700

    Add files via upload

--- egs/ema/voc1/local/load_usc_timit.py
+++ egs/ema/voc1/local/load_usc_timit.py
@@ -10,7 +10,8 @@ Created on Wed Aug 16 13:35:28 2023
 
 import os
 import numpy as np
-from scipy.io import loadmat,whosmat
+from scipy.io import loadmat
+import torchaudio
 
 from tqdm import tqdm
 
@@ -20,8 +21,12 @@ spk = 'F1'
 #directory of both MRI and EMA data
 #assumes structure: 
     #TIMIT --EMA --Data --Speaker --mat
+     #     |                      |
+     #     |                      --wav
      #     |    
      #     --MRI --Data --Speaker --tracks
+     #                            |
+     #                            --wav
 parentd = '/data'
 emad = os.path.join(parentd, 'EMA/Data')
 mrid = os.path.join(parentd,'MRI/Data')
@@ -30,6 +35,7 @@ spk_mrid = os.path.join(mrid, spk)
 
 
 #output directories for the final arrays
+#separated by wav and VT arrays
 outd = os.path.join(parentd, 'ouput')
 if not os.path.exists(outd):
     os.makedirs(outd)
@@ -39,12 +45,24 @@ if not os.path.exists(ema_outd):
 mri_outd = os.path.join(outd, 'MRI')
 if not os.path.exists(mri_outd):
     os.makedirs(mri_outd)
+ema_wav_outd = os.path.join(ema_outd, 'wav')
+if not os.path.exists(ema_wav_outd):
+    os.makedirs(ema_wav_outd)
+mri_wav_outd = os.path.join(mri_outd, 'wav')
+if not os.path.exists(mri_wav_outd):
+    os.makedirs(mri_wav_outd)
+ema_artic_outd = os.path.join(ema_outd, 'artic')
+if not os.path.exists(ema_artic_outd):
+    os.makedirs(ema_artic_outd)
+mri_artic_outd = os.path.join(mri_outd, 'artic')
+if not os.path.exists(mri_artic_outd):
+    os.makedirs(mri_artic_outd)
     
 
 #load EMA data and store as arrays of shape (# of frames, # of channels x 3) (3D datapoints)
 #each array is saved in the EMA output directory    
 ema_matd = os.path.join(spk_emad,'mat')
-out_path = os.path.join(ema_outd, 'output_paths.txt')
+out_path = os.path.join(ema_artic_outd, 'output_paths.txt')
 with open(out_path, 'w+') as ouf:
     for f in tqdm(os.listdir(ema_matd)):
         if not f.endswith('.mat'):
@@ -61,14 +79,27 @@ with open(out_path, 'w+') as ouf:
        #xyz_points.extend(list(points))    
             combined_points.append(points)
             arr = np.concatenate(combined_points,axis=1)
-            npp = os.path.join(ema_outd,f[:-4]+'.npy')
+            npp = os.path.join(ema_artic_outd,f[:-4]+'.npy')
             np.save(npp,arr)
             ouf.write('%s\n' % npp)
-
+            
+#load EMA wav files and store as arrays
+ema_wavd = os.path.join(spk_emad,'wav')
+out_path = os.path.join(ema_wav_outd, 'output_paths.txt')
+with open(out_path, 'w+') as ouf:
+    for f in tqdm(os.listdir(ema_wavd)):
+        if not f.endswith('.wav'):
+            continue
+        wavf = os.path.join(ema_wavd,f)
+        wav, _ = torchaudio.load(wavf)
+        npp = os.path.join(ema_wav_outd,f[:-4]+'.npy')
+        np.save(npp,wav)
+        ouf.write('%s\n' % npp)
+        
 #load MRI data and store as arrays of shape (# of frames, # of datapoints)
 #each array is saved in the MRI output directory
 mri_tracksd = os.path.join(spk_mrid, 'tracks')
-out_path = os.path.join(mri_outd, 'output_paths.txt')
+out_path = os.path.join(mri_artic_outd, 'output_paths.txt')
 with open(out_path, 'w+') as ouf:
     for f in tqdm(os.listdir(mri_tracksd)):
         if not f.endswith('.mat'):
@@ -90,7 +121,20 @@ with open(out_path, 'w+') as ouf:
            comb_dat.append(array)
            
         arr = np.vstack(comb_dat)
-        npp = os.path.join(mri_outd,f[:-4]+'.npy')
+        npp = os.path.join(mri_artic_outd,f[:-4]+'.npy')
         np.save(npp,arr)
+        ouf.write('%s\n' % npp)
+
+#load MRI wav files and store as arrays
+mri_wavd = os.path.join(spk_mrid,'wav')
+out_path = os.path.join(mri_wav_outd, 'output_paths.txt')
+with open(out_path, 'w+') as ouf:
+    for f in tqdm(os.listdir(mri_wavd)):
+        if not f.endswith('.wav'):
+            continue
+        wavf = os.path.join(mri_wavd,f)
+        wav, _ = torchaudio.load(wavf)
+        npp = os.path.join(mri_wav_outd,f[:-4]+'.npy')
+        np.save(npp,wav)
         ouf.write('%s\n' % npp)
     
